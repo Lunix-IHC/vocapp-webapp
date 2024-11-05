ef4ad8682f33a11ce6ca2e5d8d8d61ec2506c544
Jerson Miranda <56894248+nullbeam@users.noreply.github.com>
2024-11-05 17:53:06 -0500

    Revert "Registro usuarios"

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainComponent } from './main/main.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-
-export const routes: Routes = [
-  { path: '', component: MainComponent },
-  { path: 'signup', component: SignUpComponent },
-];

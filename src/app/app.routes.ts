612f7f4429bee67734b279a3281355fdfe5e0d49
Jerson Miranda <jerson.miranda@outlook.es>
2024-11-05 20:57:21 -0500

    feat: pantalla de creacion de  perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { MainComponent } from './main/main.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SolicitarCitaComponent } from './solicitar-cita/solicitar-cita.component';
 import { RegistroOrientadorComponent } from './registro-orientador/registro-orientador.component';
+import { PerfilEstudianteComponent } from './perfil-estudiante/perfil-estudiante.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'signup', component: SignUpComponent },
   { path: 'solicitar-cita', component: SolicitarCitaComponent },
   { path: 'registro-orientador', component: RegistroOrientadorComponent },
+  { path: 'perfil-estudiante', component: PerfilEstudianteComponent },
 ];

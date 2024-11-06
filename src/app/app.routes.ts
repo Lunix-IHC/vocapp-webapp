edc525d46c993cd85ef6f02cf74e49a5dc11fea9
Angel Diaz <agabrieldchavez@gmail.com>
2024-11-05 21:58:02 -0500

    Termine el mensaje de resultados guardados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { MainComponent } from './main/main.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { RegistroOrientadorComponent } from './registro-orientador/registro-orientador.component';
 import { AsignacionDeOrientadorComponent } from './asignacion-de-orientador/asignacion-de-orientador.component';
+import { ResultadosGuardadosComponent } from './resultados-guardados/resultados-guardados.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'signup', component: SignUpComponent },
   { path: 'registro-orientador', component: RegistroOrientadorComponent },
   { path: 'asignacion-de-orientador', component: AsignacionDeOrientadorComponent },
+  { path: 'resultados-guardados', component: ResultadosGuardadosComponent },
 ];
---
+++ src/app/resultados-guardados/resultados-guardados.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  styleUrl: './resultados-guardados.component.css',
+  selector: 'resultados-guardados',
+  standalone: true,
+  imports: [],
+  templateUrl: './resultados-guardados.component.html',
+  
+})
+export class ResultadosGuardadosComponent {}
+
+

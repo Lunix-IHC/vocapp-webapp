78b38e8fe5668c2f51cdd1e249798a1181a41f56
Angel Diaz <agabrieldchavez@gmail.com>
2024-11-05 21:24:23 -0500

    Termine con la asignacion del orientador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { RegistroOrientadorComponent } from './registro-orientador/registro-orientador.component';
+import { AsignacionDeOrientadorComponent } from './asignacion-de-orientador/asignacion-de-orientador.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'signup', component: SignUpComponent },
   { path: 'registro-orientador', component: RegistroOrientadorComponent },
+  { path: 'asignacion-de-orientador', component: AsignacionDeOrientadorComponent },
 ];

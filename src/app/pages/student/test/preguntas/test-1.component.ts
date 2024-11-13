ddd33aab3f6ee755a07339ed44550f31b53c569e
Jerson Miranda <jerson.miranda@outlook.es>
2024-11-13 13:25:36 -0500

    fix: agregado services en los test

--- src/app/pages/student/test/preguntas/test-1.component.ts
+++ src/app/pages/student/test/preguntas/test-1.component.ts
@@ -1,32 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { TestService } from '../../../../shared/services/test.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Answer } from '../../../../shared/models/answer';
 
 @Component({
   selector: 'app-test-1',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './test-1.component.html',
-  styleUrl: './test-1.component.css'
+  styleUrls: ['./test-1.component.css']
 })
-export class Test1Component {
-  step:number=1;
-  constructor(private _router: Router) { }
+export class Test1Component implements OnInit {
+  step: number = 1;
+  answers: Array<{ q1: number; q2: number; q3: number }> = [];
 
+  constructor(private _router: Router, private testService: TestService) {}
+
+  ngOnInit() {
+    this.testService.getStep().subscribe((step) => {
+      this.step = step;
+    });
+    this.testService.getAnswers().subscribe((answers) => {
+      this.answers = answers;
+    });
+  }
 
   navigateToNext() {
-    if(this.step>4){
+    console.log(this.step);
+    if (this.step < 5) {
+      this.testService.setStep(this.step + 1);
+    } else {
       this._router.navigate(['home/student/resultados-guardados']);
     }
-    else{
-      this.step++;
-    }
   }
+
   navigateToBack() {
-    if(this.step<2){
+    if (this.step > 1) {
+      this.testService.setStep(this.step - 1);
+    } else {
       this._router.navigate(['home/student/test']);
     }
-    else{
-      this.step--;
-    }
+  }
+
+  onSliderChange(question: keyof Answer, value: number) {
+    this.testService.setAnswer(this.step - 1, question, value);
   }
 }

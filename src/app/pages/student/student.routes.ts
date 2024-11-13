e7861685d20df67e73f65cad135f9f45b74d0859
Angel Diaz <agabrieldchavez@gmail.com>
2024-11-13 12:04:01 -0500

    Termine el apartado de foros

--- src/app/pages/student/student.routes.ts
+++ src/app/pages/student/student.routes.ts
@@ -6,6 +6,7 @@ import { UniversitiesComponent } from './universities/universities.component';
 import { CommunityComponent } from './community/community.component';
 import { AppointmentComponent } from './appointment/appointment.component';
 import { ResultadosGuardadosComponent } from './test/resultados-guardados/resultados-guardados.component';
+import { ForoComponent } from './foro/foro.component';
 
 export const studentRoutes: Routes = [
     {
@@ -17,6 +18,7 @@ export const studentRoutes: Routes = [
             {path: 'community', component:CommunityComponent},
             {path: 'appointment', component:AppointmentComponent},
             {path: 'resultados-guardados', component:ResultadosGuardadosComponent},
+            {path: 'foro', component:ForoComponent},
         ]
     },
 

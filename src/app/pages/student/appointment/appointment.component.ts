732f1028160afa38c27b6058dc30521d5acc3e58
Angel Diaz <agabrieldchavez@gmail.com>
2024-11-19 16:15:05 -0500

    Arregle la reserva de citas

--- src/app/pages/student/appointment/appointment.component.ts
+++ src/app/pages/student/appointment/appointment.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { AppointmentService, Advisor } from '../../../shared/services/appointment.service';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-appointment',
-  standalone: true,
-  imports: [RouterLink],
+  standalone: true, // Si el componente es standalone
+  imports: [FormsModule], // Importa FormsModule aquí
   templateUrl: './appointment.component.html',
-  styleUrl: './appointment.component.css'
+  styleUrls: ['./appointment.component.css'],
 })
 export class AppointmentComponent {
+  advisors: Advisor[] = [];
+  searchQuery: string = '';
 
+  constructor(private _router: Router, private appointmentService: AppointmentService) {}
+
+  ngOnInit(): void {
+    this.advisors = this.appointmentService.getAdvisors();
+  }
+
+  onSearch(): void {
+    this.advisors = this.appointmentService.searchAdvisors(this.searchQuery);
+  }
+
+  navigateToConfirmarCita() {
+    this._router.navigate(['home/student/Confirmacion']);
+  }
 }
